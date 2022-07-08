cb61fa484cc0485ce61d8f59984eb862c06424ea
meghasn <41339621+meghasn@users.noreply.github.com>
2022-07-08 11:49:14 +0530

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,40 @@
+console.log("Hello from JavaScript");
+var number=5;// inline comment
+/* this is a 
+multiline comment */
+
+number=9
+
+// Data types and variables
+
+// undefined, null, boolean, string, symbol, number and object
+
+// var keyword is used to declare a variable
+var myName="megha";
+myName=8;
+
+let ourName="blahblah";
+
+const pi=3.14;
+
+/*
+var can be used throughout the program
+let depends on the scope where it is declared 
+const is a constant value and should never be changed
+*/
+
+console.log(myName);
+console.log(ourName);
+console.log(pi);
+
+myName=myName+10;
+//mathematical operations
+console.log(myName);
+
+//increment a number
+myName++;
+console.log(myName);
+
+// if you want to print a quotation
+var myString="my \" string \"";
+console.log(myString);
